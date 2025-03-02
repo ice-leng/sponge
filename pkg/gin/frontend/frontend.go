87700eb66b772a1472ad15ec5514d96225018194
zhuyasen <g.zhufuyi@gmail.com>
2025-03-02 20:10:35 +0800

    feat: gin service supports static files

--- pkg/gin/frontend/frontend.go
+++ pkg/gin/frontend/frontend.go
@@ -1,4 +1,5 @@
-// Package frontend embeds the frontend static file and adds routing.
+// Package frontend is a library for serving static files in a Gin web application.
+// It supports local static files and embedding static files in binary.
 package frontend
 
 import (
@@ -7,89 +8,163 @@ import (
 	"io/fs"
 	"net/http"
 	"os"
-	"os/exec"
-	"path/filepath"
-	"runtime"
 	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-// Note: the front-end static files must all be in the same directory, i.e. htmlDir
+type options struct {
+	isUseEmbedFS    bool
+	embedFS         embed.FS
+	handleContentFn func(content []byte) []byte
+	specifiedFile   map[string]struct{}
+	is404ToHome     bool
+}
 
-// when customizing customAddr, read static resources and save them to this directory.
-var targetDir = "frontend"
+func defaultOptions() *options {
+	return &options{}
+}
+
+// Option set the jwt options.
+type Option func(*options)
+
+func (o *options) apply(opts ...Option) {
+	for _, opt := range opts {
+		opt(o)
+	}
+}
+
+// WithEmbedFS set embedFS to use embed.FS static resources.
+func WithEmbedFS(efs embed.FS) Option {
+	return func(o *options) {
+		o.isUseEmbedFS = true
+		o.embedFS = efs
+	}
+}
+
+// WithHandleContent set function to handle content and specified files
+func WithHandleContent(fn func(content []byte) []byte, files ...string) Option {
+	return func(o *options) {
+		o.handleContentFn = fn
+		if len(files) > 0 {
+			o.specifiedFile = make(map[string]struct{})
+			for _, file := range files {
+				o.specifiedFile[file] = struct{}{}
+			}
+		}
+	}
+}
+
+// With404ToHome set 404 to home page
+func With404ToHome() Option {
+	return func(o *options) {
+		o.is404ToHome = true
+	}
+}
+
+// ------------------------------------------------------------------------------------
 
 // FrontEnd is the frontend router configuration
 type FrontEnd struct {
-	// embed.FS static resources.
-	staticFS embed.FS
-	// must be set to false if cross-host access is required, otherwise true
-	isUseEmbedFS bool
-	// directory where index.html is located, e.g. web/html
-	htmlDir string
-	// configuration file in which js requests the address of the back-end api request, e.g. config.js
-	configFile string
-	// modify config
-	modifyConfigFn func(content []byte) []byte
+	//basePath string // custom prefix route path, default is empty.
+
+	sourceDir string // directory where static files is located, e.g. dist, it's also gin's route path.
+
+	isUseEmbedFS bool     // if true, use embed.FS, otherwise local static file.
+	embedFS      embed.FS // embed.FS static resources.
+
+	// only used for EmbedFS, e.g. config.js content, replace apiBaseUrl to backend api address.
+	handleContentFn func(content []byte) []byte
+	specifiedFile   map[string]struct{} // specified files to handle content, e.g. config.js
+
+	// when request route notfound
+	// true: redirect to index.html
+	// false: returns 404, default is false.
+	is404ToHome bool
 }
 
-// New create a new frontend
-func New(staticFS embed.FS, isUseEmbedFS bool, htmlDir string, configFile string, modifyConfigFn func(content []byte) []byte) *FrontEnd {
-	htmlDir = strings.Trim(htmlDir, "/")
+// New create a new frontend, default use local static file, you can use WithEmbedFS to use embed.FS.
+func New(sourceDir string, opts ...Option) *FrontEnd {
+	if sourceDir == "" {
+		sourceDir = "dist"
+	}
+
+	o := defaultOptions()
+	o.apply(opts...)
+
+	sourceDir = strings.Trim(sourceDir, "/")
 	return &FrontEnd{
-		staticFS:       staticFS,
-		isUseEmbedFS:   isUseEmbedFS,
-		htmlDir:        htmlDir,
-		configFile:     configFile,
-		modifyConfigFn: modifyConfigFn,
+		//basePath:        o.basePath,
+		sourceDir:       sourceDir,
+		isUseEmbedFS:    o.isUseEmbedFS,
+		embedFS:         o.embedFS,
+		handleContentFn: o.handleContentFn,
+		specifiedFile:   o.specifiedFile,
 	}
 }
 
 // SetRouter set frontend router
 func (f *FrontEnd) SetRouter(r *gin.Engine) error {
-	routerPath := fmt.Sprintf("%s/index.html", f.htmlDir)
-	// solve vue using history route 404 problem
-	r.NoRoute(browserRefreshFS(f.staticFS, routerPath))
-
-	relativePath := fmt.Sprintf("/%s/*filepath", f.htmlDir)
-
 	// use embed file
 	if f.isUseEmbedFS {
-		r.GET(relativePath, func(c *gin.Context) {
-			staticServer := http.FileServer(http.FS(f.staticFS))
-			staticServer.ServeHTTP(c.Writer, c.Request)
-		})
+		if f.handleContentFn == nil {
+			f.setEmbedFSRouter(r)
+		} else {
+			err := f.saveFSToLocal()
+			if err != nil {
+				return err
+			}
+			f.setLocalFileRouter(r)
+		}
 		return nil
 	}
 
 	// use local file
-	err := f.saveFSToLocal()
-	if err != nil {
-		return err
-	}
-	r.GET(relativePath, func(c *gin.Context) {
-		localFileDir := filepath.Join(targetDir, f.htmlDir)
-		filePath := c.Param("filepath")
-		c.File(localFileDir + filePath)
-	})
-
+	f.setLocalFileRouter(r)
 	return nil
 }
 
+func (f *FrontEnd) setEmbedFSRouter(r *gin.Engine) {
+	if f.is404ToHome {
+		homePage := fmt.Sprintf("%s/index.html", f.sourceDir)
+		r.NoRoute(browserRefreshFS(f.embedFS, homePage)) // solve using history route 404 problem
+	}
+
+	relativePath := fmt.Sprintf("/%s/*filepath", f.sourceDir)
+	r.GET(relativePath, func(c *gin.Context) {
+		staticServer := http.FileServer(http.FS(f.embedFS))
+		staticServer.ServeHTTP(c.Writer, c.Request)
+	})
+}
+
+func (f *FrontEnd) setLocalFileRouter(r *gin.Engine) {
+	routerPrefixPath := f.sourceDir
+	if f.is404ToHome {
+		homePage := fmt.Sprintf("%s/index.html", routerPrefixPath)
+		r.NoRoute(browserRefresh(homePage)) // solve using history route 404 problem
+	}
+
+	relativePath := fmt.Sprintf("/%s/*filepath", routerPrefixPath)
+	r.GET(relativePath, func(c *gin.Context) {
+		localFileDir := f.sourceDir
+		filePath := c.Param("filepath")
+		c.File(localFileDir + filePath)
+	})
+}
+
 func (f *FrontEnd) saveFSToLocal() error {
-	_ = os.RemoveAll(filepath.Join(targetDir, f.htmlDir))
+	_ = os.RemoveAll(f.sourceDir)
 	time.Sleep(time.Millisecond * 10)
 
 	// Walk through the embedded filesystem
-	return fs.WalkDir(f.staticFS, ".", func(path string, d fs.DirEntry, err error) error {
+	return fs.WalkDir(f.embedFS, ".", func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
 
 		// Create the corresponding directory structure locally
-		localPath := filepath.Join(targetDir, path)
+		localPath := path
 		if d.IsDir() {
 			err := os.MkdirAll(localPath, 0755)
 			if err != nil {
@@ -97,14 +172,21 @@ func (f *FrontEnd) saveFSToLocal() error {
 			}
 		} else {
 			// Read the file from the embedded filesystem
-			content, err := fs.ReadFile(f.staticFS, path)
+			content, err := fs.ReadFile(f.embedFS, path)
 			if err != nil {
 				return err
 			}
 
-			// replace config address
-			if path == f.configFile {
-				content = f.modifyConfigFn(content)
+			// handle file content
+			if len(f.specifiedFile) > 0 {
+				for file := range f.specifiedFile {
+					if strings.HasSuffix(path, file) {
+						content = f.handleContentFn(content)
+						break
+					}
+				}
+			} else {
+				content = f.handleContentFn(content)
 			}
 
 			// Save the content to the local file
@@ -138,21 +220,22 @@ func browserRefreshFS(efs embed.FS, path string) func(c *gin.Context) {
 	}
 }
 
-// AutoOpenBrowser auto open browser
-func AutoOpenBrowser(visitURL string) error {
-	var cmd string
-	var args []string
-
-	switch runtime.GOOS {
-	case "windows":
-		cmd = "cmd"
-		args = []string{"/c", "start"}
-	case "darwin":
-		cmd = "open"
-	default: // "linux", "freebsd", "openbsd", "netbsd"
-		cmd = "xdg-open"
+// solve vue using history route 404 problem, for system file
+func browserRefresh(path string) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		accept := c.Request.Header.Get("Accept")
+		flag := strings.Contains(accept, "text/html")
+		if flag {
+			content, err := os.ReadFile(path)
+			if err != nil {
+				c.Writer.WriteHeader(404)
+				_, _ = c.Writer.WriteString("Not Found")
+				return
+			}
+			c.Writer.WriteHeader(200)
+			c.Writer.Header().Add("Accept", "text/html")
+			_, _ = c.Writer.Write(content)
+			c.Writer.Flush()
+		}
 	}
-
-	args = append(args, visitURL)
-	return exec.Command(cmd, args...).Start()
 }
--- pkg/gin/frontend/frontend_test.go
+++ pkg/gin/frontend/frontend_test.go
@@ -1,46 +1,56 @@
 package frontend
 
 import (
+	"bytes"
 	"embed"
-	"os"
 	"testing"
 	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-//go:embed README.md
-var staticFS embed.FS
+func TestFrontEnd_LocalFile(t *testing.T) {
+	r := gin.New()
+	gin.SetMode(gin.ReleaseMode)
+	err := New("dist").SetRouter(r)
+	if err != nil {
+		t.Error(err)
+	}
 
-func TestFrontEnd_SetRouter(t *testing.T) {
+	r = gin.New()
+	gin.SetMode(gin.ReleaseMode)
+	err = New("dist", With404ToHome()).SetRouter(r)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+//go:embed README.md
+var embedFS embed.FS
+
+func TestFrontEnd_EmbedFS(t *testing.T) {
 	var (
-		isUseEmbedFS   = true
-		htmlDir        = "user/home"
-		configFile     = "user/home/config.js"
+		configFile     = "config.js"
 		modifyConfigFn = func(content []byte) []byte {
-			return content
+			return bytes.ReplaceAll(content, []byte("localhost"), []byte("192.168.3.37"))
 		}
 	)
 
 	r := gin.New()
 	gin.SetMode(gin.ReleaseMode)
-	err := New(staticFS, isUseEmbedFS, htmlDir, configFile, modifyConfigFn).SetRouter(r)
-	if err != nil {
-		t.Error(err)
-	}
-	time.Sleep(time.Millisecond * 100)
-
-	r = gin.New()
-	gin.SetMode(gin.ReleaseMode)
-	isUseEmbedFS = false
-	err = New(staticFS, isUseEmbedFS, htmlDir, configFile, modifyConfigFn).SetRouter(r)
+	err := New("dist", WithEmbedFS(embedFS)).SetRouter(r)
 	if err != nil {
 		t.Error(err)
 	}
 	time.Sleep(time.Millisecond * 500)
 
-	path, _ := os.Getwd()
-	err = os.RemoveAll(path + "/frontend")
+	r = gin.New()
+	gin.SetMode(gin.ReleaseMode)
+	err = New("dist",
+		WithEmbedFS(embedFS),
+		WithHandleContent(modifyConfigFn, configFile),
+		With404ToHome(),
+	).SetRouter(r)
 	if err != nil {
 		t.Error(err)
 	}
