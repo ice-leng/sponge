358e7939db424cfe0fb359d1533d654c34fd9750
zhuyasen <g.zhufuyi@gmail.com>
2025-04-06 19:40:46 +0800

    refactor: use Go AST to patch the generated Go code

--- cmd/sponge/commands/patch/common.go
+++ cmd/sponge/commands/patch/common.go
@@ -1,12 +1,25 @@
 package patch
 
 import (
+	"bufio"
 	"errors"
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 
 	"github.com/go-dev-frame/sponge/pkg/gofile"
+)
+
+const (
+	httpType = "http"
+	httpMark = "errcode.NewError"
+	grpcType = "grpc"
+	grpcMark = "errcode.NewRPCStatus"
 )
 
 // get moduleName and serverName from directory
@@ -41,7 +54,7 @@ func cutPathPrefix(srcProtoFile string) string {
 	return strings.ReplaceAll(srcProtoFile, "\\", "/")
 }
 
-func listErrCodeFiles(dir string) ([]string, error) {
+func listErrorCodeFiles(dir string) (map[string][]string, error) {
 	files, err := gofile.ListFiles(dir)
 	if err != nil {
 		return nil, err
@@ -51,17 +64,23 @@ func listErrCodeFiles(dir string) ([]string, error) {
 		return nil, errors.New("not found files")
 	}
 
-	filterFiles := []string{}
+	httpErrorCodeFiles := []string{}
+	grpcErrorCodeFiles := []string{}
 	for _, file := range files {
 		if strings.Contains(file, "systemCode_http.go") || strings.Contains(file, "systemCode_rpc.go") {
 			continue
 		}
-		if strings.Contains(file, "_http.go") || strings.Contains(file, "_rpc.go") {
-			filterFiles = append(filterFiles, file)
+		if strings.HasSuffix(file, "_http.go") {
+			httpErrorCodeFiles = append(httpErrorCodeFiles, file)
+		} else if strings.HasSuffix(file, "_rpc.go") {
+			grpcErrorCodeFiles = append(grpcErrorCodeFiles, file)
 		}
 	}
 
-	return filterFiles, nil
+	return map[string][]string{
+		httpType: httpErrorCodeFiles,
+		grpcType: grpcErrorCodeFiles,
+	}, nil
 }
 
 func getSubFiles(selectedFiles map[string][]string) []string {
@@ -72,4 +91,179 @@ func getSubFiles(selectedFiles map[string][]string) []string {
 		}
 	}
 	return subFiles
+}
+
+// ------------------------------------------------------------------------------------------
+
+func getServiceName(varNames map[string]struct{}, body string) string {
+	buf := bufio.NewReader(strings.NewReader(body))
+	for {
+		line, err := buf.ReadString('\n')
+		if err != nil {
+			break
+		}
+		line = strings.TrimSpace(line)
+		line = strings.ReplaceAll(line, " ", "")
+		if strings.Contains(line, `Name="`) {
+			sName := strings.TrimSuffix(strings.Split(line, `Name="`)[1], `"`)
+			if _, ok := varNames[sName+"NO"]; ok {
+				return sName
+			}
+		}
+	}
+	return ""
+}
+
+func findVarLineContent(varName string, src string) (srcLineCode string, valueStr string, err error) {
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset, "", src, 0)
+	if err != nil {
+		return "", "", err
+	}
+
+	lines := strings.Split(src, "\n")
+
+	for _, decl := range f.Decls {
+		genDecl, ok := decl.(*ast.GenDecl)
+		if !ok || genDecl.Tok != token.VAR {
+			continue
+		}
+
+		for _, spec := range genDecl.Specs {
+			valueSpec, ok := spec.(*ast.ValueSpec)
+			if !ok {
+				continue
+			}
+
+			for i, name := range valueSpec.Names {
+				if name.Name != varName {
+					continue
+				}
+
+				pos := name.Pos()
+				position := fset.Position(pos)
+				lineNumber := position.Line
+
+				if lineNumber < 1 || lineNumber > len(lines) {
+					return "", "", fmt.Errorf("invalid line number %d for variable %s", lineNumber, varName)
+				}
+
+				if i < len(valueSpec.Values) {
+					if bl, ok := valueSpec.Values[i].(*ast.BasicLit); ok {
+						valueStr = bl.Value
+					}
+				}
+
+				return lines[lineNumber-1], valueStr, nil
+			}
+		}
+	}
+
+	return "", "", fmt.Errorf("variable %s not found", varName)
+}
+
+type Result struct {
+	VarName        string
+	BaseCodeOffset int
+}
+
+// Visitor is a visitor that walks the AST and extracts error code offsets.
+type Visitor struct {
+	PackageName  string
+	CallFuncName string
+
+	stack   []ast.Node
+	results []Result
+}
+
+func (v *Visitor) parseParts(src string) ([]Result, error) {
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset, "", src, parser.ParseComments)
+	if err != nil {
+		return nil, err
+	}
+
+	ast.Walk(v, f)
+
+	return v.results, nil
+}
+
+// Visit implements the ast.Visitor interface.
+func (v *Visitor) Visit(node ast.Node) ast.Visitor {
+	if node == nil {
+		if len(v.stack) > 0 {
+			v.stack = v.stack[:len(v.stack)-1]
+		}
+		return nil
+	}
+
+	v.processNode(node)
+	v.stack = append(v.stack, node)
+	return v
+}
+
+func (v *Visitor) isNewErrorCall(callExpr *ast.CallExpr) bool {
+	if selExpr, ok := callExpr.Fun.(*ast.SelectorExpr); ok {
+		if ident, ok := selExpr.X.(*ast.Ident); ok && ident.Name == v.PackageName {
+			return selExpr.Sel.Name == v.CallFuncName
+		}
+	}
+	return false
+}
+
+// nolint
+func (v *Visitor) processNode(node ast.Node) {
+	// handling calls
+	callExpr, ok := node.(*ast.CallExpr)
+	if !ok || !v.isNewErrorCall(callExpr) {
+		return
+	}
+
+	// get variable name
+	var varName string
+	for i := len(v.stack) - 1; i >= 0; i-- {
+		switch n := v.stack[i].(type) {
+		case *ast.ValueSpec:
+			for _, value := range n.Values {
+				if value == callExpr {
+					if len(n.Names) > 0 {
+						varName = n.Names[0].Name
+					}
+					break
+				}
+			}
+		case *ast.AssignStmt:
+			for _, rhs := range n.Rhs {
+				if rhs == callExpr {
+					if len(n.Lhs) > 0 {
+						if ident, ok := n.Lhs[0].(*ast.Ident); ok {
+							varName = ident.Name
+						}
+					}
+					break
+				}
+			}
+		}
+		if varName != "" {
+			break
+		}
+	}
+
+	baseOffset := 0
+	if len(callExpr.Args) > 0 {
+		if binExpr, ok := callExpr.Args[0].(*ast.BinaryExpr); ok && binExpr.Op == token.ADD {
+			if yLit, ok := binExpr.Y.(*ast.BasicLit); ok && yLit.Kind == token.INT {
+				if val, err := strconv.Atoi(yLit.Value); err == nil {
+					baseOffset = val
+				}
+			}
+		}
+	}
+
+	if varName != "" && baseOffset != 0 {
+		v.results = append(v.results, Result{
+			VarName:        varName,
+			BaseCodeOffset: baseOffset,
+		})
+	}
 }
--- cmd/sponge/commands/patch/modify-dup-err-code.go
+++ cmd/sponge/commands/patch/modify-dup-err-code.go
@@ -1,49 +1,46 @@
 package patch
 
 import (
-	"bufio"
-	"bytes"
 	"fmt"
 	"os"
-	"regexp"
-	"strconv"
+	"sort"
 	"strings"
 
 	"github.com/fatih/color"
 	"github.com/spf13/cobra"
+
+	"github.com/go-dev-frame/sponge/pkg/goast"
 )
 
-// ModifyDuplicateErrCodeCommand modify duplicate error codes
-func ModifyDuplicateErrCodeCommand() *cobra.Command {
-	var (
-		dir string
-	)
+// ModifyDuplicateErrorCodeOffsetCommand modify duplicate error code offset command
+func ModifyDuplicateErrorCodeOffsetCommand() *cobra.Command {
+	var dir string
 
 	cmd := &cobra.Command{
 		Use:   "modify-dup-err-code",
-		Short: "Modify duplicate error codes",
-		Long:  "Modify duplicate error codes.",
-		Example: color.HiBlackString(`  # Modify duplicate error codes
+		Short: "Modify duplicate error code offset",
+		Long:  "Modify duplicate error code offset.",
+		Example: color.HiBlackString(`  # Modify duplicate error code offset
   sponge patch modify-dup-err-code --dir=internal/ecode`),
 		SilenceErrors: true,
 		SilenceUsage:  true,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			files, err := listErrCodeFiles(dir)
+			filesMap, err := listErrorCodeFiles(dir)
 			if err != nil {
-				return err
+				fmt.Println("listErrCodeFiles:", err)
+				return nil
 			}
 
-			var total int
-			for _, file := range files {
-				count, err := checkAndModifyDuplicateErrCode(file)
-				if err != nil {
-					return err
+			for _, files := range filesMap {
+				for _, file := range files {
+					err = checkAndModifyDuplicateErrorCodeOffset(file)
+					if err != nil {
+						fmt.Println(err)
+						return nil
+					}
 				}
-				total += count
 			}
-			if total > 0 {
-				fmt.Println("modify duplicate error codes successfully.")
-			}
+
 			return nil
 		},
 	}
@@ -53,122 +50,229 @@ func ModifyDuplicateErrCodeCommand() *cobra.Command {
 	return cmd
 }
 
-type eCodeInfo struct {
-	Name   string
-	Num    int
-	Str    string
-	DstStr string
-}
-
-var (
-	serviceGroupSeparatorMark = "// ---------- Do not delete or move this split line, this is the merge code marker ----------"
-	defineHTTPErrCodeMark     = "errcode.NewError("
-	defineGRPCErrCodeMark     = "errcode.NewRPCStatus("
-)
-
-func parseErrCodeInfo(line string) eCodeInfo {
-	ci := eCodeInfo{}
-
-	pattern := `(\w+)\s*=\s*\w+\.(.*?)\((.*?),`
-	re := regexp.MustCompile(pattern)
-	match := re.FindStringSubmatch(line)
-	if len(match) < 4 {
-		return ci
-	}
-	baseCodeStr := match[3]
-
-	index := strings.Index(line, baseCodeStr)
-	if index < 0 {
-		return ci
-	}
-	srcStr := line[:index] + baseCodeStr
-
-	ss := strings.Split(baseCodeStr, "+")
-	if len(ss) != 2 {
-		return ci
-	}
-	num, _ := strconv.Atoi(strings.TrimSpace(ss[1]))
-
-	ci.Name = match[1]
-	ci.Num = num
-	ci.Str = srcStr
-	ci.DstStr = line[:index] + ss[0] + "+"
-
-	return ci
-}
-
-func getModifyCodeInfos(codes []eCodeInfo) ([]eCodeInfo, int) {
-	maxCode := 0
-	m := map[int][]eCodeInfo{}
-
-	for _, ci := range codes {
-		if ci.Num > maxCode {
-			maxCode = ci.Num
-		}
-
-		if cis, ok := m[ci.Num]; ok {
-			m[ci.Num] = append(cis, ci)
-		} else {
-			m[ci.Num] = []eCodeInfo{ci}
-		}
-	}
-
-	needModify := []eCodeInfo{}
-	for _, infos := range m {
-		if len(infos) > 1 {
-			needModify = append(needModify, infos[1:]...)
-		}
-	}
-
-	return needModify, maxCode
-}
-
-func modifyErrCode(data []byte, infos []eCodeInfo, maxCode int) []byte {
-	for _, info := range infos {
-		maxCode++
-		data = bytes.ReplaceAll(data, []byte(info.Str), []byte(info.DstStr+strconv.Itoa(maxCode)))
-	}
-	return data
-}
-
-func getDuplicateErrCodeInfo(data []byte) ([]eCodeInfo, int) {
-	cis := []eCodeInfo{}
-
-	buf := bufio.NewReader(bytes.NewReader(data))
-	for {
-		line, err := buf.ReadString('\n')
-		if err != nil {
-			break
-		}
-		if !strings.Contains(line, defineHTTPErrCodeMark) && !strings.Contains(line, defineGRPCErrCodeMark) {
-			continue
-		}
-
-		ci := parseErrCodeInfo(line)
-		if ci.Name != "" {
-			cis = append(cis, ci)
-		}
-	}
-
-	return getModifyCodeInfos(cis)
-}
-
-func checkAndModifyDuplicateErrCode(file string) (int, error) {
-	data, err := os.ReadFile(file)
+func checkAndModifyDuplicateErrorCodeOffset(file string) error {
+	codeAst, err := NewErrorCodeOffsetAst(file)
 	if err != nil {
-		return 0, err
+		return fmt.Errorf("NewErrorCodeOffsetAst error: %v, file: %s", err, file)
 	}
 
-	serviceGroupData := bytes.Split(data, []byte(serviceGroupSeparatorMark))
-	var fileContent [][]byte
-	var count int
-	for _, groupData := range serviceGroupData {
-		ecis, maxCode := getDuplicateErrCodeInfo(groupData)
-		fileContent = append(fileContent, modifyErrCode(groupData, ecis, maxCode))
-		count += len(ecis)
+	isNeedSave := false
+	for _, codeInfo := range codeAst.ErrorCodeNameInfoMap {
+		codeAst.SrcCode, isNeedSave = codeInfo.CheckDuplicateErrorCodeOffset(codeAst.SrcCode)
 	}
 
-	data = bytes.Join(fileContent, []byte(serviceGroupSeparatorMark))
-	err = os.WriteFile(file, data, 0666)
-	return count, err
+	if isNeedSave && codeAst.SrcCode != "" {
+		return os.WriteFile(codeAst.FilePath, []byte(codeAst.SrcCode), 0666)
+	}
+
+	return nil
+}
+
+// ErrorCodeOffsetAst is the struct for error code offset
+type ErrorCodeOffsetAst struct {
+	FilePath string
+	SrcCode  string
+	FileSize int
+
+	ErrorCodeNameInfoMap map[string]*ErrorCodeOffset
+}
+
+// ErrorCodeOffset is the struct for error code offset
+type ErrorCodeOffset struct {
+	Name string
+
+	OffsetInfos []*Deconstruction
+	offsetIndex int
+
+	VarNameCodeSrcMap map[string]string // varName -> varErrCodeSrc
+	Body              string
+}
+
+// Deconstruction is the struct for error code offset deconstruction
+type Deconstruction struct {
+	VarName    string
+	VarValue   int
+	VarSrcCode string
+}
+
+// NewErrorCodeOffsetAst create a ErrorCodeOffsetAst object
+func NewErrorCodeOffsetAst(filePath string) (*ErrorCodeOffsetAst, error) {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	astInfos, err := goast.ParseGoCode(filePath, data)
+	if err != nil {
+		return nil, err
+	}
+
+	var errorCodeOffsetInfoMap = map[string]*ErrorCodeOffset{}
+
+	for _, info := range astInfos {
+		if info.Type == goast.VarType {
+			spliceType := 0
+			if strings.Contains(info.Body, httpMark) {
+				spliceType = 1
+			} else if strings.Contains(info.Body, grpcMark) {
+				spliceType = 2
+			}
+
+			if spliceType > 0 {
+				var varNames = map[string]struct{}{}
+				for _, varName := range info.Names {
+					if strings.HasSuffix(varName, "NO") {
+						if spliceType == 2 {
+							varName = strings.TrimPrefix(varName, "_")
+						}
+						varNames[varName] = struct{}{}
+					}
+				}
+
+				serviceName := getServiceName(varNames, info.Body)
+
+				if serviceName == "" {
+					continue
+				}
+
+				//varNOName := serviceName + "NO"
+				callFuncName := "NewError"
+				infoBody := "package ecode\n\nimport (\"github.com/go-dev-frame/sponge/pkg/errcode\")\n\n" + info.Body
+				if spliceType == 2 {
+					//varNOName = "_" + varNOName
+					callFuncName = "NewRPCStatus"
+				}
+
+				vis := &Visitor{PackageName: "errcode", CallFuncName: callFuncName}
+				results, err := vis.parseParts(infoBody)
+				if err != nil {
+					return nil, fmt.Errorf("parseParts error: %v", err)
+				}
+
+				var errCodes []*Deconstruction
+				var varErrMap = make(map[string]string)
+				for _, result := range results {
+					varErrCodeSrc, _, err := findVarLineContent(result.VarName, infoBody)
+					if err != nil {
+						return nil, fmt.Errorf("findVarLineContent error: %v", err)
+					}
+					errCodes = append(errCodes, &Deconstruction{
+						VarName:    result.VarName,
+						VarValue:   result.BaseCodeOffset,
+						VarSrcCode: varErrCodeSrc,
+					})
+					varErrMap[result.VarName] = varErrCodeSrc
+				}
+				if len(errCodes) != len(results) || len(errCodes) == 0 {
+					continue
+				}
+				errCodesIndex := len(errCodes) - 1
+
+				errorCodeOffsetInfoMap[serviceName] = &ErrorCodeOffset{
+					Name:              serviceName,
+					OffsetInfos:       errCodes,
+					offsetIndex:       errCodesIndex,
+					VarNameCodeSrcMap: varErrMap,
+					Body:              info.Body,
+				}
+			}
+		}
+	}
+
+	if len(errorCodeOffsetInfoMap) == 0 {
+		return nil, fmt.Errorf("no error code found")
+	}
+
+	return &ErrorCodeOffsetAst{
+		FilePath:             filePath,
+		SrcCode:              string(data),
+		FileSize:             len(data),
+		ErrorCodeNameInfoMap: errorCodeOffsetInfoMap,
+	}, nil
+}
+
+// CheckDuplicateErrorCodeOffset check if the error code offset has duplicate value
+func (c *ErrorCodeOffset) CheckDuplicateErrorCodeOffset(srcCode string) (string, bool) {
+	varValueMap := make(map[int][]string)
+	duplicateNOMap := make(map[int][]string)
+	maxVarValue := 0
+
+	for _, r := range c.OffsetInfos {
+		varValueMap[r.VarValue] = append(varValueMap[r.VarValue], r.VarName)
+		if r.VarValue > maxVarValue {
+			maxVarValue = r.VarValue
+		}
+	}
+
+	for offset, names := range varValueMap {
+		if len(names) > 1 {
+			duplicateNOMap[offset] = names
+		}
+	}
+
+	if len(duplicateNOMap) == 0 {
+		return srcCode, false
+	}
+
+	return c.replaceErrorCodeOffset(srcCode, duplicateNOMap, maxVarValue)
+}
+
+func (c *ErrorCodeOffset) replaceErrorCodeOffset(srcCode string, duplicateNOMap map[int][]string, maxVarValue int) (string, bool) {
+	isModified := false
+
+	var sortNOs []int
+	for varValue := range duplicateNOMap {
+		sortNOs = append(sortNOs, varValue)
+	}
+	sort.Ints(sortNOs)
+
+	for _, varValue := range sortNOs {
+		varNames := duplicateNOMap[varValue]
+		for i, name := range varNames {
+			if i > 0 {
+				oldStr := c.VarNameCodeSrcMap[name]
+				maxVarValue++
+				if maxVarValue > 99 {
+					maxVarValue = 99
+				}
+				newStr := strings.ReplaceAll(oldStr, fmt.Sprintf("BaseCode+%d,", varValue), fmt.Sprintf("BaseCode+%d,", maxVarValue))
+				srcCode = strings.ReplaceAll(srcCode, oldStr, newStr)
+				isModified = true
+			}
+		}
+	}
+
+	return srcCode, isModified
+}
+
+// CheckMergedItems check if the merged items have duplicate error code offset
+func (c *ErrorCodeOffset) CheckMergedItems(srcCode string, codeInfo *ErrorCodeOffset) (string, bool) {
+	if c.Name != codeInfo.Name {
+		return srcCode, false
+	}
+
+	var newErrCodes []*Deconstruction
+	if codeInfo != nil && len(codeInfo.OffsetInfos) > 0 {
+		for _, errCode := range codeInfo.OffsetInfos {
+			if _, ok := c.VarNameCodeSrcMap[errCode.VarName]; !ok {
+				newErrCodes = append(newErrCodes, errCode)
+			}
+		}
+	}
+	if len(newErrCodes) == 0 {
+		return srcCode, false
+	}
+
+	var varSrcCodes []string
+	for _, errCode := range newErrCodes {
+		c.OffsetInfos = append(c.OffsetInfos, errCode)
+		c.VarNameCodeSrcMap[errCode.VarName] = errCode.VarSrcCode
+		varSrcCodes = append(varSrcCodes, errCode.VarSrcCode)
+	}
+
+	markerStr := c.OffsetInfos[c.offsetIndex].VarSrcCode
+	joinStr := markerStr + "\n" + strings.Join(varSrcCodes, "\n")
+	srcCode = strings.ReplaceAll(srcCode, markerStr, joinStr)
+
+	return srcCode, true
 }
