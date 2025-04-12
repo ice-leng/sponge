ef814cf96ae1c39c49f895a6417409a4b19d5609
zhuyasen <g.zhufuyi@gmail.com>
2025-04-12 11:33:33 +0800

    feat: print error message according to error code

--- pkg/grpc/interceptor/logging_test.go
+++ pkg/grpc/interceptor/logging_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/go-dev-frame/sponge/pkg/errcode"
 	"github.com/go-dev-frame/sponge/pkg/logger"
 )
 
@@ -14,7 +15,7 @@ func TestUnaryClientLog(t *testing.T) {
 	time.Sleep(time.Millisecond * 200)
 	cli := newUnaryRPCClient(addr,
 		UnaryClientRequestID(),
-		UnaryClientLog(logger.Get(), WithReplaceGRPCLogger()),
+		UnaryClientLog(logger.Get(), WithReplaceGRPCLogger(), WithPrintErrorByCodes(errcode.StatusInvalidParams.Code())),
 	)
 	_ = sayHelloMethod(context.Background(), cli)
 }
@@ -23,6 +24,15 @@ func TestUnaryServerLog(t *testing.T) {
 	addr := newUnaryRPCServer(
 		UnaryServerRequestID(),
 		UnaryServerLog(logger.Get(), WithReplaceGRPCLogger()),
+	)
+	time.Sleep(time.Millisecond * 200)
+	cli := newUnaryRPCClient(addr)
+	_ = sayHelloMethod(context.Background(), cli)
+}
+
+func TestUnaryServerSimpleLog(t *testing.T) {
+	addr := newUnaryRPCServer(
+		UnaryServerRequestID(),
 		UnaryServerSimpleLog(logger.Get(), WithReplaceGRPCLogger()),
 	)
 	time.Sleep(time.Millisecond * 200)
@@ -45,12 +55,11 @@ func TestUnaryServerLog_ignore(t *testing.T) {
 	addr := newUnaryRPCServer(
 		UnaryServerLog(logger.Get(),
 			WithMaxLen(200),
-			WithLogFields(map[string]interface{}{"foo": "bar"}),
 			WithMarshalFn(func(reply interface{}) []byte {
 				data, _ := json.Marshal(reply)
 				return data
 			}),
-			WithLogIgnoreMethods("/api.user.v1.user/GetByID"),
+			WithLogIgnoreMethods("/proto.Greeter/SayHello"),
 		),
 	)
 	time.Sleep(time.Millisecond * 200)
@@ -63,11 +72,9 @@ func TestStreamServerLog(t *testing.T) {
 		StreamServerRequestID(),
 		StreamServerLog(logger.Get(),
 			WithReplaceGRPCLogger(),
-			WithLogFields(map[string]interface{}{}),
 		),
 		StreamServerSimpleLog(logger.Get(),
 			WithReplaceGRPCLogger(),
-			WithLogFields(map[string]interface{}{}),
 		),
 	)
 	time.Sleep(time.Millisecond * 200)
